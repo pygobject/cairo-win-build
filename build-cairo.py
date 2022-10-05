8567faffe22e5190c685758c22a0bd24ee587132
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 20:16:33 +0000

    Adding tarfile member sanitization to extractall()

--- build-cairo.py
+++ build-cairo.py
@@ -264,7 +264,26 @@ def download_and_extract(
     if not extract_dir.exists() or not extract_dir.is_dir():
         log.info("Extracting archive")
         with tarfile.open(tararchive) as tar:
-            tar.extractall(destdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, destdir)
         if not extract_dir.exists():
             shutil.move(
                 list(Path(destdir).glob("*-*-*"))[0],
